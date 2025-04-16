8feb177254c860b9bc1bbe7c0b53dc3a8d5679ef
abbykrug <hse2dq@virginia.edu>
2025-04-16 17:01:43 -0400

    add bubbles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [SerializeField] private ParticleSystem bubbles;
     public float moveSpeed = 5f;
     private Vector2 moveV;
     private Animator animator;
+    private Vector2 particleStartPos;
+    private ParticleSystem.EmissionModule emission;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        particleStartPos = bubbles.transform.localPosition;
+        emission = bubbles.emission;
     }
 
     // Update is called once per frame
@@ -23,12 +28,21 @@ public class PlayerMovement : MonoBehaviour
         moveV = moveInput.normalized * moveSpeed;
         if (Input.GetAxisRaw("Horizontal") != 0) {
             animator.SetFloat("X", (Input.GetAxisRaw("Horizontal")));
+            emission.rateOverTime = 20;
             //animator.SetFloat("Y", (Input.GetAxisRaw("Vertical")));
             //animator.SetBool("isRunning", true);
+            if (Input.GetAxisRaw("Horizontal") > 0){
+                bubbles.transform.localPosition = particleStartPos;
+                bubbles.transform.rotation = Quaternion.Euler(0,0,0);
+            }else{
+                Vector2 bubblesPos = particleStartPos;
+                bubblesPos.x *= -1f;
+                bubbles.transform.localPosition = bubblesPos;
+                bubbles.transform.rotation = Quaternion.Euler(0,180,0);
+            }
+        }else{
+            emission.rateOverTime = 3;
         }
-        //else{
-            //animator.SetBool("isRunning", false);
-        //}
         
     }
 
